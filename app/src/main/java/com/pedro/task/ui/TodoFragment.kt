3f4ddd6f26067c88d5e834de15f2dd9a0b32546e
20241TIIMI0097 <souzapedroalmeida8@gmail.com>
2026-08-20 09:01:47 -0300

    Feito até o passo 108

--- app/src/main/java/com/pedro/task/ui/TodoFragment.kt
+++ app/src/main/java/com/pedro/task/ui/TodoFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.pedro.task.R
 import com.pedro.task.databinding.FragmentTodoBinding
 
@@ -20,6 +21,17 @@ class TodoFragment : Fragment() {
     ): View {
         _binding = FragmentTodoBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+    }
+
+    private fun initListeners(){
+        binding.floatingActionButton2.setOnClickListener {
+            findNavController().navigate((R.id.action_homeFragment_to_formTaskFragment))
+        }
     }
 
     override fun onDestroyView() {
