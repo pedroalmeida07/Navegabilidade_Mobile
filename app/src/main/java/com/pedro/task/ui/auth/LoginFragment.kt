245e7eff1dadb5d0d19a280771e685ec87407a13
pedroalmeida07 <souzapedroalmeida8@gmail.com>
2026-08-26 14:39:56 -0300

    Feito até ponto 121 (falta testar o ponto 111, 118 e 121)

--- app/src/main/java/com/pedro/task/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/pedro/task/ui/auth/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.pedro.task.R
 import com.pedro.task.databinding.FragmentLoginBinding
@@ -41,6 +42,20 @@ class LoginFragment : Fragment() {
 
         binding.btnRecover.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment2_to_recoverAccountFragment3)
+        }
+    }
+
+    private fun validateData(){
+        val email = binding.campoEmail.text.toString().trim()
+        val senha = binding.campoSenha.text.toString().trim()
+        if (email.isNotBlank()){
+            if (senha.isNotBlank()){
+                findNavController().navigate(R.id.action_global_homeFragment)
+            } else{
+                Toast.makeText(requireContext(), "Preencha a senha!", Toast.LENGTH_SHORT).show()
+            }
+        } else{
+            Toast.makeText(requireContext(), "Preencha seu email!", Toast.LENGTH_SHORT).show()
         }
     }
 
