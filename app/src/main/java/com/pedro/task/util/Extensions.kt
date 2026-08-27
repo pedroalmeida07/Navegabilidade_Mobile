8356c17df634afd055230ba2e1ac3faee19da5fa
pedroalmeida07 <souzapedroalmeida8@gmail.com>
2026-08-27 17:23:34 -0300

    Feito até ponto 150

--- app/src/main/java/com/pedro/task/util/Extensions.kt
+++ app/src/main/java/com/pedro/task/util/Extensions.kt
@@ -21,17 +21,20 @@ fun Fragment.initToolbar(toolbar: Toolbar){
 fun Fragment.showBottomSheet(
     titleDialog: Int? = null,
     titleButton: Int? = null,
-    message: Int,
+    message: String,
     onClick: () -> Unit ={}
 ){
-    val bottomSheetDialog = BottomSheetDialog(requireContext())
+    val bottomSheetDialog = BottomSheetDialog(requireContext(), R.style.bottomSheetDialog)
     val binding: BottomSheetBinding = BottomSheetBinding.inflate(layoutInflater, null, false)
 
     binding.textViewTitle.text = getText(titleDialog ?: R.string.text_title_warning)
-    binding.textViewMessage.text = getText(message)
+    binding.textViewMessage.text = message
     binding.buttonOk.text = getText(titleButton ?: R.string.text_button_warning)
     binding.buttonOk.setOnClickListener {
         onClick()
         bottomSheetDialog.dismiss()
     }
+
+    bottomSheetDialog.setContentView(binding.root)
+    bottomSheetDialog.show()
 }
